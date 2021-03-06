297b35b76a6684508712aa2b37a15d28b5e0fd8f
Eoin Thomas O'Hehir <mail@etohehir.com>
2021-03-05 21:26:40 -0800

    Scrolling was broken due to an outdated call. It's fixed now

--- assets/javascript/master.js
+++ assets/javascript/master.js
@@ -14,16 +14,17 @@ var dnlswan = new function() {
     if ((t /= d / 2) < 1) return c / 2 * t * t * t * t + b;
     return -c / 2 * ((t -= 2) * t * t * t - 2) + b;
   }
-  this.scrollTo = function (element, destination, duration) {
+  this.scrollTo = function (destination, duration) {
     // Scrolls the window to a destination over time
-    if (typeof(element) != 'undefined' && element != null || duration > 0) {
-      var origin = element.scrollTop;
+    if (typeof(window) !== 'undefined' && window !== null || duration > 0) {
+      var origin = window.scrollY;
       var start = new Date().getTime();
 
       var timer = setInterval(function() {
+        console.log('[LOG] interval is running')
         var time = new Date().getTime() - start;
         var currPosition = self.easeInOutQuart(time, origin, destination - origin, duration);
-        element.scrollTop = currPosition;
+        window.scrollTo(0, currPosition);
         if (time >= duration) clearInterval(timer);
       }, 16.67);
     }
@@ -32,30 +33,30 @@ var dnlswan = new function() {
   // Applied functions
   this.scrollBelowFold = function() {
     var scrollDownButton = document.querySelector('.hero-down_arrow');
-    if (typeof(scrollDownButton) != 'undefined' && scrollDownButton != null) {
+    if (typeof(scrollDownButton) !== 'undefined' && scrollDownButton !== null) {
       scrollDownButton.onclick = function() {
         var destination = document.querySelector('.hero').offsetHeight;
-        self.scrollTo(document.body, destination, speed);
+        self.scrollTo(destination, speed);
       }
     }
   };
   this.scrollToWork = function(){
     var workAnchor = document.querySelector('a.nav-link.work');
-    if (typeof(workAnchor) != 'undefined' && workAnchor != null) {
+    if (typeof(workAnchor) !== 'undefined' && workAnchor !== null) {
       workAnchor.onclick = function(evt) {
         evt.preventDefault();
         var destination = document.getElementById('work').getBoundingClientRect().top;
-        self.scrollTo(document.body, destination, speed);
+        self.scrollTo(destination, speed);
       }
     }
   };
   this.scrollToAbout = function() {
     var aboutAnchor = document.querySelector('a.nav-link.about');
-    if (typeof(aboutAnchor) != 'undefined' && aboutAnchor != null) {
+    if (typeof(aboutAnchor) !== 'undefined' && aboutAnchor !== null) {
       aboutAnchor.onclick = function(evt) {
         evt.preventDefault();
         var destination = document.querySelector('section.about-section').getBoundingClientRect().top;
-        self.scrollTo(document.body, destination, speed);
+        self.scrollTo(destination, speed);
       }
     }
   }
